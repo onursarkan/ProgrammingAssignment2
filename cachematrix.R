6b7e7d7f21948e6bf7e391d71e568f80919f84a0
Onur Sarkan <onursarkan@gmail.com>
2015-02-22 15:41:36 +0200

    s This is homework 2 (lexical scoping) for Coursera R programming course :wq
    :wq:

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming Course
+## by Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
 
-## Write a short comment describing this function
+## Programming Assignment 2: Lexical Scoping
 
+## By this assignment, new Caching mechanism is developed for matrix inverse operations.
+## This R code contains 2 main functions: makeCacheMatrix, cacheSolve
+
+
+## makeCacheMatrix: By this function, In-Memory caching object(list with getter and setter) is produced for input matrix. 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialize cache with null value
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  ## This function return the input matrix:
+  get <- function() x
+  
+  ## This is setter function for caching mechanism:
+  setsolve <- function(solve) s <<- solve
+  
+  ## This is getter function for caching mechanism:
+  getsolve <- function() s
+  
+  ## This is complex output data type with caching capability 
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+  
+## cacheSolve: This function produce inverse matrix for previous function's outputs.
+## If there is a cached result, it returns from cache.
+## If there is no cached result, it calculate the inverse matrix, also it caches the result, and than returns the results.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## It checks the cached value:
+  s <- x$getsolve()
+  
+  ## If there is a cached value, returns this inverse matrix.
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  ## If there is no cached inverse matrix, it takes the input matrix:
+  data <- x$get()
+  
+  ## It produce inverse matrix for input matrix:
+  s <- solve(data, ...)
+  
+  ## Set the cache with calculated inverse matrix:
+  x$setsolve(s)
+  
+  ## It returns newly calculated inverse matrix:
+  s
 }
